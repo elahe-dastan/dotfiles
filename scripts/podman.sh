60ada6478cff11a1080451ff8c83a77ab8c8bdb7
Elahe Dastan <elahe.dstn@gmail.com>
2022-07-15 18:14:32 +0430

    feat: add podman installation for osx

--- scripts/podman.sh
+++ scripts/podman.sh
@@ -27,7 +27,11 @@ main_brew() {
 	podman machine start || true
 
 	msg "podman and dive configuration"
+<<<<<<< HEAD
 	configfile containers registries.conf podman
+=======
+	configfile containers "" podman
+>>>>>>> 330e1956 (feat: add podman installation for osx)
 }
 
 main_pacman() {
@@ -36,6 +40,12 @@ main_pacman() {
 
 	require_aur hadolint-bin lazydocker-bin
 
+<<<<<<< HEAD
+=======
+	msg "install lazydocker with yay"
+	yay -Syu --needed --noconfirm lazydocker-bin
+
+>>>>>>> 330e1956 (feat: add podman installation for osx)
 	msg "podman and dive configuration"
 	configfile dive "" podman
 	configfile containers "" podman
