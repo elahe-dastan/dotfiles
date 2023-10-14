212dc93ef9b3e20073d94b9c46ad6237b59a320e
Parham Alvani <parham.alvani@gmail.com>
2023-10-14 19:42:12 +0000

    feat: remove unused modules

--- scripts/browser.sh
+++ scripts/browser.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export dependencies=('buku')
-
 usage() {
 	echo "install firefox, firefox-developer-edition and tridactyl configuration"
 	echo '
@@ -15,46 +13,5 @@ usage() {
 }
 
 main_brew() {
-	require_brew_cask firefox
-
-	gopass-jsonapi configure --browser firefox
-}
-
-main_pacman() {
-	require_pacman firefox w3m firefox-developer-edition
-
-	if [[ "$(command -v gopass-jsonapi)" ]]; then
-		gopass-jsonapi configure --browser firefox
-	fi
-
-	if [[ "$(command -v bukubrow)" ]]; then
-		bukubrow --install-firefox
-	fi
-
-	msg 'set default browser using xdg-settings'
-	bash xdg-settings set default-web-browser firefox.desktop
-
-	copycat "browser" "firefox/autoconfig.js" "/usr/lib/firefox/defaults/pref/autoconfig.js"
-	copycat "browser" "firefox/firefox.cfg" "/usr/lib/firefox/firefox.cfg"
-
-	if yes_or_no 'browser' 'do you want to install vivaldi?'; then
-		require_pacman vivaldi vivaldi-ffmpeg-codecs
-	fi
-
-	if yes_or_no 'browser' 'do you want to install edge?'; then
-		require_aur microsoft-edge-dev-bin
-	fi
-
-	if yes_or_no 'browser' 'do you want to install chrome?'; then
-		msg 'chrome, the worst browser ever but sometime we need that shit'
-		require_aur google-chrome-beta
-
-		if [[ "$(command -v gopass-jsonapi)" ]]; then
-			gopass-jsonapi configure --browser chrome --path ~/.config/google-chrome-beta --manifest-path ~/.config/google-chrome-beta/NativeMessagingHosts/com.justwatch.gopass.json
-		fi
-
-		if [[ "$(command -v bukubrow)" ]]; then
-			bukubrow --install-chrome --install-dir ~/.config/google-chrome-beta/NativeMessagingHosts/
-		fi
-	fi
+	require_brew_cask google-chrome
 }
